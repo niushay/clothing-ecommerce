2cbccf74622a29b7009f86589311863a5f543ca5
niusha-yousefi <niushayousefi73@gmail.com>
2022-08-03 19:11:40 +0300

    Directory and Category item components

---
+++ src/components/directory/directory.components.jsx
@@ -0,0 +1,16 @@
+import './directory.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+const Directory = ({ categories }) => {
+
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            )
+            )}
+        </div>
+    )
+}
+
+export default Directory;
